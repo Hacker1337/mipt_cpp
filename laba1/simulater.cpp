0186d1022b7fe1cc8376268f7530cbedd62a9e66
Hacker1337 <amirfvb@gmail.com>
2022-02-18 18:38:13 +0300

    working modeling module

---
+++ laba1/simulater.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Амир on 18.02.2022.
+//
+
+#include "simulater.h"
---
+++ laba1/simulater.h
@@ -0,0 +1,8 @@
+//
+// Created by Амир on 18.02.2022.
+//
+
+#ifndef MIPT_CPP_SIMULATER_H
+#define MIPT_CPP_SIMULATER_H
+
+#endif //MIPT_CPP_SIMULATER_H
