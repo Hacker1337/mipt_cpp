49fe670fa02dc3aeb8b9f0192d2c228fc12e6ff5
Hacker1337 <amirfvb@gmail.com>
2022-10-26 13:14:14 +0300

    Good memory treatment in laba 4

--- 3_sem/laba4/grids.cpp
+++ 3_sem/laba4/grids.cpp
@@ -6,7 +6,7 @@ class Grid {  // if dim != 2, create massive of dim-1 dimensional Grids
    private:
     Grid<T, dim - 1>* data;
     size_type z_size;
-
+    Grid() = default;   // default constructor for arrays initialization
    public:
     template <typename... Args>
     T& operator()(size_type z_idx,
@@ -21,19 +21,50 @@ class Grid {  // if dim != 2, create massive of dim-1 dimensional Grids
         return data[z_idx](args...);
     }
 
-    template <typename... Args>
-    Grid(size_type z_size,
-         Args... args)  // multidimensional constructor by dimensions.
-        : z_size(z_size) {
-        data = new Grid<T, dim - 1>(args...);
-        for (int i = 1; i < z_size; i++) {
-            data[i] = Grid<T, dim - 1>(args...);
-        }
-    }
     Grid<T, dim - 1>& operator[](size_type z_idx) { return data[z_idx]; }
 
     Grid<T, dim - 1> const& operator[](size_type z_idx) const {
         return data[z_idx];
+    }
+
+    template <typename... Args>
+    Grid(size_type z_size,
+         Args... args)  // multidimensional constructor by dimensions.
+        : z_size(z_size) {
+        data = new Grid<T, dim - 1>[z_size];
+        for (int i = 0; i < z_size; i++) {
+            data[i] = Grid<T, dim - 1>(args...);
+        }
+    }
+
+    ~Grid() { delete[] data; }
+    Grid(Grid<T, dim> const& grid) : z_size(grid.z_size) {
+        data = new Grid<T, dim - 1>[z_size];
+
+        for (size_type z_idx = 0; z_idx != z_size; ++z_idx)
+            data[z_idx] = grid[z_idx];
+    }
+
+    Grid<T>& operator=(Grid<T> const& grid) {
+        this->z_size = grid.z_size;
+        delete[] data;
+        data = new Grid<T, dim-1>[z_size];
+        for (size_type z_idx = 0; z_idx != z_size; ++z_idx)
+                data[z_idx] = grid[z_idx];
+        return *this;
+    }
+
+    Grid(Grid<T>&& grid) : z_size(grid.z_size) {
+        data = grid.data;
+        grid.data = nullptr;
+    }
+
+    Grid<T>& operator=(Grid<T>&& grid) {
+        this->z_size = grid.z_size;
+        delete[] data;
+        data = grid.data;
+        grid.data = nullptr;
+        return *this;
     }
 
     size_type get_z_size() const { return z_size; }
@@ -48,8 +79,8 @@ class Grid<T, 2> final {
    private:
     T* data;
     size_type y_size, x_size;
-
    public:
+    Grid() = default; // default constructor for arrays initialization
     Grid(T* data, size_type y_size, size_type x_size)
         : data(data), y_size(y_size), x_size(x_size) {}
 
@@ -65,7 +96,7 @@ class Grid<T, 2> final {
 
     ~Grid() { delete[] data; }
 
-    Grid(Grid<T> const& grid) : Grid(grid.z_size, grid.y_size) {
+    Grid(Grid<T> const& grid) : Grid(grid.y_size, grid.x_size) {
         for (size_type y_idx = 0; y_idx != y_size; ++y_idx)
             for (size_type x_idx = 0; x_idx != x_size; ++x_idx)
                 (*this)(y_idx, x_idx) = grid(y_idx, x_idx);
